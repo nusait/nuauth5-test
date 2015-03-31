0ff8baded7ded86cd00bc421a2c71dce13bd099e
Hao Luo <howlowck@gmail.com>
2015-03-31 16:39:54 -0500

    initial commit

---
+++ config/nuauth.php
@@ -0,0 +1,8 @@
+<?php
+return [
+	'autoCreate' => false,
+	'netidColumn' => 'netid',
+	'firstNameColumn' => 'first_name',
+	'lastNameColumn' => 'last_name',
+	'emailColumn' => 'email'
+];
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('netid')->unique();
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('password')->nullable();
+			$table->string('email')->unique();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
